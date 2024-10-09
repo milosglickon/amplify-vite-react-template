53e1e4e1d6e56d63c65a2bcb574df9946e69a33e
Milos Nikolic <milos.nikolic@glickon.com>
2024-10-09 13:41:18 +0200

    added authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-
-Amplify.configure(outputs);
+import React from "react"
+import { I18n } from "aws-amplify/utils"
+import ReactDOM from "react-dom/client"
+import { Authenticator } from "@aws-amplify/ui-react"
+import App from "./App.tsx"
+import "./index.css"
+import { Amplify } from "aws-amplify"
+import outputs from "../amplify_outputs.json"
+import "@aws-amplify/ui-react/styles.css"
+import { translations } from "@aws-amplify/ui-react"
+I18n.putVocabularies(translations)
+I18n.setLanguage("it")
+Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    {/* <Authenticator socialProviders={["amazon", "apple", "facebook", "google"]}> */}
+    <Authenticator>
+      <App />
+    </Authenticator>
+  </React.StrictMode>,
+)
